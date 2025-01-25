9e5814ac28662aafa9180e0f43500951ef913f7a
Kenil Vora <kenilvora2509@gmail.com>
2025-01-25 15:25:14 +0530

    Minor Changes

--- server/src/routes/PatientRoute.ts
+++ server/src/routes/PatientRoute.ts
@@ -1,7 +1,5 @@
 import express from "express";
-import { auth, isPatient } from "../middlewares/auth";
-import { updatePatientProfile } from "../controllers/Patient";
 
 const router = express.Router();
 
-router.put("/update-profile", auth, isPatient, updatePatientProfile);
+export default router;
